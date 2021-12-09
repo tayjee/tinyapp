a78ac94c5ab43e900223e3742ca630b7bc86c09d
Jimmy Tang <jimmy.tang.360@gmail.com>
2021-12-09 21:15:18 +0000

    Added a second route with template

--- express_server.js
+++ express_server.js
@@ -18,6 +18,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
