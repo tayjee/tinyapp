21eee54082a5235f4807dd192e7ec1bc1ecef7f2
Jimmy Tang <jimmy.tang.360@gmail.com>
2021-12-10 02:49:07 +0000

    Incorporated entries to the urlDatabase object when user inputs a longURL and also implemented redirection if /u/shorturl

--- express_server.js
+++ express_server.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+//Function that generates a random alphanumeric string combination.
+function generateRandomString() {
+  //Array to hold all the alphanumeric values (including both lower and upper case).
+    const alphaNumeric = [
+    'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e',
+    'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j',
+    'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o',
+    'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't',
+    'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y',
+    'Z', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    //Empty array to push the random string into
+    const array = [];
+    //Loop with the Math.random function to determine the alphaNumeric index to be pushed into the combined string
+    for (let i = 0; i < 6; i++) { 
+        array.push(alphaNumeric[Math.round(Math.random() * 62)]);
+    }
+    //Joins all the seperate characters into a singular random alphanumeric string.
+    return array.join('');
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -18,9 +39,29 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  let short = generateRandomString()
+  urlDatabase[`${short}`] = req.body.longURL
+  console.log(req.body.longURL);
+  res.redirect(`/urls/${short}`);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+ if (longURL != undefined) {
+  res.redirect(longURL);
+ } else {
+  res.send("404 Error Page Not Found")
+ }
 });
 
 app.get("/urls.json", (req, res) => {
