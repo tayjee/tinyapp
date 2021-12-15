2ac13349000822cfeb091b837d13961f7ee4eda2
Jimmy Tang <jimmy.tang.360@gmail.com>
2021-12-15 15:57:03 +0000

    Cleaned up the code, fixed some indentation

--- express_server.js
+++ express_server.js
@@ -1,44 +1,53 @@
+//Neccessities
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bcrypt = require('bcryptjs');
 const bodyParser = require("body-parser");
-const cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
 const {generateRandomString, matchingEmail, getUserByEmail, urlsForUser } = require('./helpers.js');
+
+app.set("view engine", "ejs");
+
+//Middleware
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2']
 }));
-app.set("view engine", "ejs");
 
+//Object to hold the longURL associated with a shortURL and also the user who created the shortened URL.
 const urlDatabase = {
   "b6UTxQ": {
-      longURL: "https://www.tsn.ca",
-      userID: "test1234"
+    longURL: "https://www.tsn.ca",
+    userID: "test1234"
   },
   "Zw54dW": {
     longURL: "https://github.com",
     userID: "test4321"
   },
   "i3BoGr": {
-      longURL: "https://www.google.ca",
-      userID: "test1234"
+    longURL: "https://www.google.ca",
+    userID: "test1234"
   }
 };
 
+//Object to hold information on the users (id, email, password).
 const users = {
   "abcdefg" : {
     id: 'abcdefg',
     email: 'test@test.com',
-    password: 'test' 
+    password: 'test'
   }
 };
 
+
+//Routes
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//Main page for users to see the shortened URLs that they created.
 app.get("/urls", (req, res) => {
   const userID = req.session.user_id;
   const database = urlDatabase;
@@ -47,9 +56,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+
 app.post("/urls", (req, res) => {
   let userID = req.session.user_id;
-  if(userID === undefined) {
+  if (userID === undefined) {
     res.redirect('/login');
   } else {
     let short = generateRandomString(6);
@@ -59,17 +69,19 @@ app.post("/urls", (req, res) => {
   }
 });
 
+//Page for user to create a new shortened URL.
 app.get("/urls/new", (req, res) => {
   let userID = req.session.user_id;
   const templateVars = { user: users[userID] };
 
-  if(userID === undefined) {
+  if (userID === undefined) {
     res.redirect('/login');
   } else {
     res.render("urls_new", templateVars);
   }
 });
 
+//Registration page
 app.get("/register", (req, res) => {
   let userID = req.session.user_id;
   const templateVars = { user: users[userID] };
@@ -87,7 +99,7 @@ app.post('/register', (req, res) => {
   } else {
     const id = generateRandomString(8);
     users[id] = {id, email, hashedPassword};
-    let userID = users[id].id
+    let userID = users[id].id;
     req.body.user_id = userID;
     res.redirect('/urls');
   }
@@ -99,15 +111,17 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+//If user paths to this destination they get redirected to the long URL.
 app.get("/u/:shortURL", (req, res) => {
   let redirectURL = urlDatabase[req.params.shortURL].longURL;
-  if (redirectURL != undefined) {
+  if (redirectURL !== undefined) {
     res.redirect(redirectURL);
   } else {
     res.send("404 Error Page Not Found");
   }
 });
 
+//Function that allows users to delete shortened URL entries that they created.
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   let userID = req.session.user_id;
@@ -120,6 +134,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+//Page that allows user to edit the destination of their short URL.
 app.post('/urls/:id', (req, res) => {
   const shortURL = req.params.id;
   let userID = req.session.user_id;
@@ -133,16 +148,17 @@ app.post('/urls/:id', (req, res) => {
   res.redirect('/urls');
 });
 
+//Page that shows the url database in json format.
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//Filler page
 app.get("/hello", (req, res) => {
-  let userID = req.session.user_id;
   const templateVars = { greeting: 'Hello World!' };
   res.render("hello_world", templateVars);
 });
-
+//Login page
 app.get("/login", (req, res) => {
   let userID = req.session.user_id;
   const templateVars = { user: users[userID] };
@@ -152,22 +168,21 @@ app.get("/login", (req, res) => {
 app.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const hashedPassword = getUserByEmail(email, users).hashedPassword
+  const hashedPassword = getUserByEmail(email, users).hashedPassword;
   const compareSync = bcrypt.compareSync(password, hashedPassword);
   const login = getUserByEmail(email, users);
-  const id = login.id
 
   if (!login || compareSync === false) {
     res.send("Incorrect Username or Password");
     return;
   }
 
-  req.session.user_id = login.id
+  req.session.user_id = login.id;
   res.redirect('/urls');
 });
 
+//Clears cookies and session on logout.
 app.post('/logout', (req, res) => {
-  const id = req.body.user_id;
   req.session = null;
   res.clearCookie('user_id');
   res.redirect('/urls');
